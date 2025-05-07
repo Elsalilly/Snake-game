5b3ba8abe1a822b8d9b31b4f39377b6c66665856
Elsalilly <elsa.mertala@gmail.com>
2025-05-07 08:14:51 +0200

    move snake functionality added

--- src/components/snake.jsx
+++ src/components/snake.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import "../styles/game.css";
 
 const Snake = ({ snakeDots }) => {
-
+{/* Loop through each coordinate in snakeDots array -> renders a square + coordinates in %*/}
     return (
         <div>
             {snakeDots.map((dot, i) => {
                 const style = {
-                    left: `${dot[0]}%`,
-                    top: `${dot[1]}%`,
+                    left: `${dot[0]}%`, //horizontal positon
+                    top: `${dot[1]}%`, //vertical position
                 };
-            return <div className="snake" key={i} style={style} />;
+            return ( 
+            <div 
+                className="snake"
+                key={i}
+                style={style}
+                />
+            );
             })}
         </div>
     );
-}
+};
 
 export default Snake;
--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,12 +1,15 @@
+//Components
 import Snake from "../components/snake";
 import Food from "../components/food";
 import Buttons from "../components/buttons";
 
+// CSS
 import "../styles/game.css";
 
-import React, { useState } from "react";
+//React and useState
+import React, { useEffect, useState } from "react";
 
-//Random food coordinates 
+//Random food coordinates so it's within game grid 
 const getRandomCoordinates = () => {
     let min = 1;
     let max = 98;
@@ -20,13 +23,69 @@ const initialState = {
     snakeDots: [[0, 0], [2,0]],
     food: getRandomCoordinates(),
     direction: "RIGHT",
-    speed: 400,
+    speed: 200,
 }
 
 const Game = () => {
-const [snakeDots, setSnakeDots] = useState(initialState.snakeDots);
-const [food, setFood] = useState(initialState.food);
-const [speed, setSpeed] = useState(initialState.speed);
+    const [snakeDots, setSnakeDots] = useState(initialState.snakeDots); //State to track snake's position
+    const [food, setFood] = useState(initialState.food); //State to track food location 
+    const [speed, setSpeed] = useState(initialState.speed); //State to track snake speed
+    const [direction, setDirection] = useState(initialState.direction); // State to track direction
+
+    //Handles keyboard input, snake direction
+    useEffect(() => {
+        onkeydown = (e) => {
+            switch (e.key) {
+                case "ArrowUp":
+                    setDirection("UP");
+                    break;
+                case "ArrowDown":
+                    setDirection("DOWN");
+                    break;
+                case "ArrowLeft":
+                    setDirection("LEFT");
+                    break;
+                case "ArrowRight":
+                    setDirection("RIGHT");
+                    break;
+                default:
+                    break;
+        }
+    };
+    
+    document.addEventListener("keydown", onkeydown); //Event listener for keyboard input
+    const gameInterval = setInterval(moveSnake, speed);
+
+    return () => {
+        clearInterval(gameInterval);
+        document.removeEventListener("keydown", onkeydown);
+    };
+}, [snakeDots, direction]);
+
+    const moveSnake = () => {
+        let dots = [...snakeDots];
+        let head = dots[dots.length -1];
+
+        switch (direction) {
+            case "UP":
+                head = [head[0], head[1] - 2];
+                break;
+            case "DOWN":
+                head = [head[0], head[1] + 2];
+                break;
+            case "LEFT":
+                head = [head[0] - 2, head[1]];
+                break;
+            case "RIGHT":
+                head = [head[0] + 2, head[1]];
+                break;
+            default:
+                break;
+        }
+        dots.push(head);
+        dots.shift();
+        setSnakeDots(dots);
+    };
 
     return (
        <div className="game-area">
